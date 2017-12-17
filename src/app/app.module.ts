315858f7874782c5390893b443d672d3e23efbfb
billy westbury <billy.westbury@gmail.com>
2017-12-17 08:56:01 +0000

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,12 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
+import { AgmCoreModule } from '@agm/core';
+
+
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+
 
 @NgModule({
   declarations: [
@@ -14,7 +18,10 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AgmCoreModule.forRoot({
+      apiKey: 'key'
+    })
   ],
   bootstrap: [IonicApp],
   entryComponents: [
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,13 +2,22 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+  selector: "page-home",
+  templateUrl: "home.html"
 })
 export class HomePage {
+  title: string = "My first AGM project";
 
-  constructor(public navCtrl: NavController) {
+  // home coords =  {lat: 55.87687269106657, lng: -4.193703532218933}
+  lat: number = 55.87687269106657;
+  lng: number = -4.193703532218933;
+  locationChoosen =  false;
+  constructor(public navCtrl: NavController) {}
 
+  onChooseLocation(event) {
+    console.log(event);
+    this.lat = event.coords.lat;
+    this.lng = event.coords.lng;
+    this.locationChoosen = true;
   }
-
 }
